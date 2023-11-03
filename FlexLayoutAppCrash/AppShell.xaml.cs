a7956e4c95aac84c02e4cac78e56d47c733edf05
William Bohrmann <wbohrmann@esri.com>
2023-11-03 12:03:43 -0700

    Add project files.

---
+++ FlexLayoutAppCrash/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FlexLayoutAppCrash
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FlexLayoutAppCrash/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FlexLayoutAppCrash
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
